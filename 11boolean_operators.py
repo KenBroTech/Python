1813e8363b875b0c2cbc1f827b8bd9270ab5bdef
KenBroTech <80349696+KenBroTech@users.noreply.github.com>
2022-03-01 14:47:54 +0000

    Update and rename 11boolean_operators.py to 11other_operators.py

--- 11boolean_operators.py
+++
@@ -1,9 +0,0 @@
-# Python boolean operators [ and or not]
-# Boolean operators are used to combine conditional statements
-
-
-# and
-
-# or
-
-# not
---
+++ 11other_operators.py
@@ -0,0 +1,16 @@
+# Python operators 
+# Boolean operators are used in combination with other conditional statements and operators
+
+# Membership Operators
+# in 
+
+# not in
+
+
+# Logical Operators 
+
+# and
+
+# or
+
+# not
